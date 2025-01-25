a7b2de6cbd4828379e662e43d108447031e9835b
Gregory D <GrandAdmiralGergar@users.noreply.github.com>
2025-01-25 16:50:32 -0500

    Some guessing on how to fix the locators on ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import allWords from './data/words_alpha_five.json'
 import allPatterns from './data/allPatterns.json'
